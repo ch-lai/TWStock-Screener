37dfc6a6d15f2d68ffbbfe660a28c59622bd1e53
ch-lai <andy1994723@gmail.com>
2019-09-13 22:25:14 +0800

    Add seasonal report

--- crawl_report.py
+++ crawl_report.py
@@ -48,6 +48,39 @@ def monthly(year, month):
     return df
 
 
+def seasonal(year, season, type='綜合損益彙總表'):
+
+    # 假如是西元，轉成民國
+    if year > 1990:
+        year -= 1911
+
+    if type == '綜合損益彙總表':
+        url = 'https://mops.twse.com.tw/mops/web/ajax_t163sb04'
+    elif type == '資產負債彙總表':
+        url = 'https://mops.twse.com.tw/mops/web/ajax_t163sb05'
+    elif type == '營益分析彙總表':
+        url = 'https://mops.twse.com.tw/mops/web/ajax_t163sb06'
+    else:
+        print('type does not match')
+
+    r = requests.post(url, {
+        'encodeURIComponent':1,
+        'step':1,
+        'firstin':1,
+        'off':1,
+        'TYPEK':'sii',
+        'year':str(year),
+        'season':str(season),
+    })
+
+    r.encoding = 'utf8'
+    dfs = pd.read_html(r.text, header=None)
+
+    return pd.concat(dfs[1:], axis=0, sort=False)\
+             .set_index(['公司代號'])\
+             .apply(lambda s: pd.to_numeric(s, errors='ceorce'))
+
+
 def cumulative(n_months, load_report, save_report):
     data = {}
 
