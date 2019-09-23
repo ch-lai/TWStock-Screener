e2953e4053a1f6b0640363e31a598aada20d427a
ch-lai <andy1994723@gmail.com>
2019-09-24 00:25:42 +0800

    Add QOR and OPM_NPM screener

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 #########################################################
-# 選股步驟:
-# 1. 根據月營收成長篩選
-# 2. 再用季財報過濾出體質更好的標的
-# 3. 評分法 0.8 < (EPS*殖利率)/PER(本益比) < 1.2
-# 4. 其他方法(ROE, 殖利率)
-# 
-# 
 # TODO-list:
 # 1. 評分法 0.8 < (EPS*殖利率)/PER(本益比) < 1.2
 # 2. 比較兩表相同的ID
@@ -13,6 +6,10 @@
     # targets = short_one
     # (coo[:, None] == targets).any(1)
 # 3. 4季財報成長法
+# 4. 90% < 營業利益（損失）/(營業利益（損失）+ 營業外收入及支出) < 110%
+# 5. ROE > 8%
+# 6. 爬 "綜合損益彙總表"，並加入feature
+# 7. 選出後，畫出近一個月K線圖，並結合技術指標提供買點
 #########################################################
 
 from stock_screener import StockScreenerMonth
@@ -20,7 +17,7 @@ from stock_screener import StockScreenerSeason
 
 
 # Init StockScreener type
-ss = StockScreenerMonth()
+# ss = StockScreenerMonth()
 ss = StockScreenerSeason()
 
 # Select multiple screener methods
